c84b4a6605e763912191d08692b7adaab41169b6
hptme <hpttme@gmail.com>
2013-10-02 15:04:49 +0800

    f2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Learning::Application.config.secret_key_base = '5c4d6a20382ba6974dfd4cd60958dd42ca1962b264f78bb95fff2c6634eebeab84de1f4c9eb01a459142ad99e6d5be2f5ab0d5a62a9a46ed52fd8def1bdaf7d2'
